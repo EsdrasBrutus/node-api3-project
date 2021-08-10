d078a1a8b929132bb0d24ef6628a69d16a2834ab
GenHero <51133244+GenHero@users.noreply.github.com>
2021-08-10 18:50:29 -0400

    completed middleware functions

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,17 +1,50 @@
 function logger(req, res, next) {
-  // DO YOUR MAGIC
+  console.log(
+    `[${new Date().toISOString()}] ${req.method} to ${req.url} from ${req.get(
+      'Origin'
+    )}`
+  );
+
+  next();
 }
 
-function validateUserId(req, res, next) {
-  // DO YOUR MAGIC
+const validateUserId = async (req, res, next) =>{
+  const {id} = req.params
+  try {
+    const user = await Posts.getById(id)
+    if(!user){
+      res.status(404).json({message: ""})
+    }
+    else{
+      req.user = user
+      next();
+    }
+  }
+  catch(error){
+    res.status(500).json({message: ""})
+  }
 }
 
 function validateUser(req, res, next) {
-  // DO YOUR MAGIC
+  if(!req.body.name){
+    res.status(400).json({ message: "missing required name field" })
+  }else{
+    next()
+  }
 }
 
 function validatePost(req, res, next) {
-  // DO YOUR MAGIC
+  if(!req.body.text){
+    res.status(400).json({ message: "missing required text field" })
+  }else{
+    next()
+  }
 }
 
+module.exports = {
+  logger,
+  validateUserId,
+  validateUser,
+  validatePost
+}
 // do not forget to expose these functions to other modules
